d38a82754e3ca84a41f1ef7c33082664e515da16
Serhii <sergonito@gmail.com>
2024-01-29 09:21:36 +0100

    done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
+import './sass/main.scss';
+import tesla from './assets/tesla.jpg';
+
+const mainImage = document.getElementById("mainImage")
+mainImage.src = tesla
+
 const mult = (a, b) => a * b;
 
 console.log(mult(2,4));
 console.log(mult(3,5));
+
+const hello = import('./js/hello');
+
+console.log('all ok!');
+
+//alert('ok123');
+
+// test 123
+
